150df688c526613ed236cb66d822d1473bdd726e
Elena Vygovska <elena.vygovska@gmail.com>
2019-05-26 21:17:11 +0300

    Added deploy.sh

---
+++ script/deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin ;"
+docker tag zutherb/monolithic-shop:latest evygovska/monolithic:latest
+docker push evygovska/monolithic:latest
