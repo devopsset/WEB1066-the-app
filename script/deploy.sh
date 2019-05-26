231fa33b76459d1efb1c092dd6b4df9849e01c47
Elena Vygovska <elena.vygovska@gmail.com>
2019-05-26 23:05:42 +0300

    Another fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin ;"
 docker pull zutherb/monolithic-shop
-docker tag zutherb/monolithic-shop:latest \"${DOCKER_LOGIN}\"/monolithic:latest
-docker push \"${DOCKER_LOGIN}\"/monolithic:latest
+docker tag zutherb/monolithic-shop:latest evygovska/monolithic:latest
+docker push evygovska/monolithic:latest
